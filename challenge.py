54ddbc0210d80d84ef55e84493bc038d3c7d4a7d
7Hero <vpoopescu07@gmail.com>
2021-06-10 17:56:35 +0300

    com2

--- challenge.py
+++ challenge.py
@@ -1,43 +1,24 @@
+#!/usr/bin/python3
+import os
+flag = open('flag.txt', 'r').read().strip().encode()
 
+class XOR:
+    def __init__(self):
+        self.key = os.urandom(4)
+    def encrypt(self, data: bytes) -> bytes:
+        xored = b''
+        for i in range(len(data)):
+            xored += bytes([data[i] ^ self.key[i % len(self.key)]])
+        return xored
+    def decrypt(self, data: bytes) -> bytes:
+        return self.encrypt(data)
 
+def main():
+    global flag
+    crypto = XOR()
+    print ('Flag:', crypto.encrypt(flag).hex())
 
-key = []
-for x in range(256):
-    if(int("13",16) ^ x == ord("H")):
-        key.append(x)
-        break
-for x in range(256):
-    if(int("4a",16) ^ x == ord("T")):
-        key.append(x)
-        break
-for x in range(256):
-    if(int("f6",16) ^ x == ord("B")):
-        key.append(x)
-        break
-for x in range(256):
-    if(int("e1",16) ^ x == ord("{")):
-        key.append(x)
-        break
-flag = '134af6e1297bc4a96f6a87fe046684e8047084ee046d84c5282dd7ef292dc9'
-arr = []
-for n in range(len(flag)):
-    if(n%2==0):
-        mr = int(flag[n]+flag[n+1],16)
-        arr.append(mr)
-xored = b""
-for i in range(len(arr)):
-    xored += bytes([arr[i] ^ key[i % 4]])
-
-print(xored)
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
 
 
